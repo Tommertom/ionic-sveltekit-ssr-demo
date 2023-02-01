5e6232898caf3b78352626981d642954f0ffb2e7
Tommertom <Tom.Gruintjes@gmail.com>
2023-02-01 10:59:21 +0100

    SW debugging

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,17 +3,45 @@
 	import './styles.css';
 
 	// we do implicit registration of service worker using the Readable API in pwa.store.ts
-	import { listenForBeforeInstallPrompt, needRefresh, updateServiceWorker } from '$lib/pwa.store';
-	import { onMount } from 'svelte';
-	if ($needRefresh)
-		// aggressive update of app
-		setTimeout(() => {
-			updateServiceWorker(true);
-		}, 4000);
+	// import { listenForBeforeInstallPrompt, needRefresh, updateServiceWorker } from '$lib/pwa.store';
+	// import { onMount } from 'svelte';
+	// if ($needRefresh)
+	// 	// aggressive update of app
+	// 	setTimeout(() => {
+	// 		updateServiceWorker(true);
+	// 	}, 4000);
 
-	onMount(() => {
-		listenForBeforeInstallPrompt();
+	// onMount(() => {
+	// 	listenForBeforeInstallPrompt();
+	// });
+
+	import { onMount } from 'svelte';
+	import { pwaInfo } from 'virtual:pwa-info';
+
+	onMount(async () => {
+		if (pwaInfo) {
+			const { registerSW } = await import('virtual:pwa-register');
+			registerSW({
+				immediate: true,
+				onRegistered(r) {
+					// uncomment following code if you want check for updates
+					// r && setInterval(() => {
+					//    console.log('Checking for sw update')
+					//    r.update()
+					// }, 20000 /* 20s for testing purposes */)
+					console.log(`SW Registered: ${r}`);
+				},
+				onRegisterError(error) {
+					console.log('SW registration error', error);
+				}
+			});
+		}
 	});
+
+	$: webManifest = pwaInfo ? pwaInfo.webManifest.linkTag : '';
 </script>
 
+<svelte:head>
+	{@html webManifest}
+</svelte:head>
 <slot />
