b7450fd8545ead88e2e5f9b61cc29c16eef7ac87
Tommertom <Tom.Gruintjes@gmail.com>
2023-02-01 16:56:53 +0100

    Finetuning UI

--- src/routes/app/install/+page.svelte
+++ src/routes/app/install/+page.svelte
@@ -15,28 +15,30 @@
 	}
 </script>
 
-<h1>Install page</h1>
+<ion-content>
+	<h1>Install page</h1>
 
-This page should run all code needed to install the PWA on the homescreen. This involves a.o.:
+	This page should run all code needed to install the PWA on the homescreen. This involves a.o.:
 
-<ul>
-	<li>checking for browser compatability</li>
-	<li>android or iOS install</li>
-	<li>monitor for beforeinstallprompt event (if applicable)</li>
-	<li>keeping user busy while waiting for it</li>
-	<li>show relevant UI for installation</li>
-	<li>telling the user what to do once installed</li>
-</ul>
+	<ul>
+		<li>checking for browser compatability</li>
+		<li>android or iOS install</li>
+		<li>monitor for beforeinstallprompt event (if applicable)</li>
+		<li>keeping user busy while waiting for it</li>
+		<li>show relevant UI for installation</li>
+		<li>telling the user what to do once installed</li>
+	</ul>
 
-<br /><br />
-A magic button will appear here if the browser supports beforeinstallprompt.
-<br />
-{#if $beforeinstallprompt !== undefined}
-	<ion-button on:keypress={runInstallEvent} on:click={runInstallEvent}
-		>You can install via button!</ion-button
-	>
-{:else}
-	<ion-spinner />
-{/if}
-<br />
-If not, then manually go to splash. <a href="/app/splash">Go to splash page</a>
+	<br /><br />
+	A magic button will appear here if the browser supports beforeinstallprompt.
+	<br />
+	{#if $beforeinstallprompt !== undefined}
+		<ion-button on:keypress={runInstallEvent} on:click={runInstallEvent}
+			>You can install via button!</ion-button
+		>
+	{:else}
+		<ion-spinner />
+	{/if}
+	<br />
+	If not, then manually go to splash. <a href="/app/splash">Go to splash page</a>
+</ion-content>
--- src/routes/app/login/+page.svelte
+++ src/routes/app/login/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Your login page</h1>
-Login or signup here.
+<ion-content>
+	<h1>Your login page</h1>
+	Login or signup here.
+</ion-content>
