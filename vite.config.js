1a6604054706d45ec3f027a916d291feea728b90
Tommertom <Tom.Gruintjes@gmail.com>
2023-02-01 09:59:22 +0100

    PWA assets

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,30 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { SvelteKitPWA } from '@vite-pwa/sveltekit'
-import { pwaConfiguration } from './pwa-configuration';
 
+const pwaManifest = {
+	name: "Ionic SSR demo",
+	short_name: "IonicSSRDemo",
+	description: "Ionic SSR demo",
+	theme_color: '#f27649',
+	orientation: "portrait",
+	icons: [
+		{
+			"src": "/android-chrome-192x192.png",
+			"sizes": "192x192",
+			"type": "image/png",
+			"purpose": "maskable any"
+		}
+	]
+}
+
+const pwaConfiguration = {
+	srcDir: './build',
+	outDir: './build', // to ensure sw.js and workbox are in build - not according to docs, but works nevertheless? 
+	includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png', 'dist/*'],
+	base: '/',
+	scope: '/',
+	manifest: pwaManifest
+}
 
 /** @type {import('vite').UserConfig} */
 const config = {
